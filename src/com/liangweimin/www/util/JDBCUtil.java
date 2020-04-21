db5594e9b400fdaac7b350bb791730c1658b2e4f
Liangwm-hub <1633962872@qq.com>
2020-04-21 16:48:44 +0800

    1.01

--- src/com/liangweimin/www/util/JDBCUtil.java
+++ src/com/liangweimin/www/util/JDBCUtil.java
@@ -2,36 +2,66 @@ package com.liangweimin.www.util;
 
 import java.sql.*;
 
+/**
+ * @author 梁伟民
+ */
 public class JDBCUtil {
 
-    //获取连接
+    /**
+     * 获取连接
+     *
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        String url="jdbc:mysql://localhost:3306/appointments?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF8&useSSL=false";
-        String user="root";
-        String password="369258258Aa@";
+        String url = "jdbc:mysql://localhost:3306/appointments?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF8&useSSL=false";
+        String user = "root";
+        String password = "369258258Aa@";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn= DriverManager.getConnection(url,user,password);
+        Connection conn = DriverManager.getConnection(url, user, password);
 
         return conn;
     }
 
-    //关闭资源
-    public static void closeSource(Connection conn, Statement ps, ResultSet rs){
+    /**
+     * 关闭资源
+     *
+     * @param conn
+     * @param ps
+     * @param rs
+     */
+    public static void closeSource(Connection conn, Statement ps, ResultSet rs) {
         try {
-            if (conn!=null) conn.close();
-            if (ps!=null) ps.close();
-            if (rs!=null) rs.close();
+            if (conn != null) {
+                conn.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    //关闭资源
-    public static void closeSource(Connection conn, Statement ps){
+    /**
+     * 关闭资源
+     *
+     * @param conn
+     * @param ps
+     */
+    public static void closeSource(Connection conn, Statement ps) {
         try {
-            if (conn!=null) conn.close();
-            if (ps!=null) ps.close();
+            if (conn != null) {
+                conn.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
